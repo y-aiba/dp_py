e17cad3734b6df99fbe0389ebc39a86e8c447bc3
yaiba <yukihiro.aiba@gmail.com>
2021-09-13 22:27:54 +0900

    mainはクラスじゃなくていい

--- adapter/sample1/main.py
+++ adapter/sample1/main.py
@@ -1,14 +1,11 @@
 from adapter.sample1.print_banner import Print, PrintBanner
 
 
-class Main:
-    @staticmethod
-    def main():
-        p: Print = PrintBanner(string="Hello")
-        p.print_weak()
-        p.print_strong()
+def main():
+    p: Print = PrintBanner(string="Hello")
+    p.print_weak()
+    p.print_strong()
 
 
 if __name__ == '__main__':
-    m = Main()
-    m.main()
+    main()
--- factory_method/sample/main.py
+++ factory_method/sample/main.py
@@ -1,21 +1,18 @@
 from factory_method.sample.id_card_factory import IDCardFactory, Product
 
 
-class Main:
-    @staticmethod
-    def main():
-        # factoryをFactory型にするとget_ownersがないという警告が出る。
-        factory: IDCardFactory = IDCardFactory()
-        card1: Product = factory.create('Aさん')
-        card2: Product = factory.create('Bさん')
-        card3: Product = factory.create('Cさん')
-        card1.use()
-        card2.use()
-        card3.use()
-        owners = factory.get_owners()
-        print(owners)
+def main():
+    # factoryをFactory型にするとget_ownersがないという警告が出る。
+    factory: IDCardFactory = IDCardFactory()
+    card1: Product = factory.create('Aさん')
+    card2: Product = factory.create('Bさん')
+    card3: Product = factory.create('Cさん')
+    card1.use()
+    card2.use()
+    card3.use()
+    owners = factory.get_owners()
+    print(owners)
 
 
 if __name__ == '__main__':
-    m = Main()
-    m.main()
+    main()
