2bdf5afccf9c96a56f6656a9607008c8c908389a
yaiba <yukihiro.aiba@gmail.com>
2021-09-13 22:19:23 +0900

    Factory method sample微修正

--- factory_method/sample/id_card_factory.py
+++ factory_method/sample/id_card_factory.py
@@ -6,14 +6,14 @@ from typing import List
 
 class IDCardFactory(Factory):
     def __init__(self):
-        self._owners: List = []
+        self.owners: List = []
 
     def create_product(self, owner: str) -> Product:
         return IDCard(owner=owner)
 
     # ここjavaだと引数がProduct
     def register_product(self, id_card: IDCard) -> None:
-        self._owners.append(id_card.get_owner())
+        self.owners.append(id_card.get_owner())
 
     def get_owners(self) -> List[str]:
-        return self._owners
+        return self.owners
--- factory_method/sample/main.py
+++ factory_method/sample/main.py
@@ -1,16 +1,19 @@
-from factory_method.sample.id_card_factory import Factory, IDCardFactory, Product
+from factory_method.sample.id_card_factory import IDCardFactory, Product
 
 
 class Main:
     @staticmethod
     def main():
-        factory: Factory = IDCardFactory()
+        # factoryをFactory型にするとget_ownersがないという警告が出る。
+        factory: IDCardFactory = IDCardFactory()
         card1: Product = factory.create('Aさん')
         card2: Product = factory.create('Bさん')
         card3: Product = factory.create('Cさん')
         card1.use()
         card2.use()
         card3.use()
+        owners = factory.get_owners()
+        print(owners)
 
 
 if __name__ == '__main__':
